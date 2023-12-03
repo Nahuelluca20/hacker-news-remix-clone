d6af14efa63c84b0ce280a216fe3d7b21f3731e7
Nahuel Luca <nahuelluca20@gmail.com>
2023-12-03 11:27:07 -0300

    Add expand/collapse functionality to comment text

--- app/components/comment.tsx
+++ app/components/comment.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { timeAgo } from "~/utils/timeAgo";
 import { IComment } from "~/utils/types";
 
 export default function Comment({ comment }: { comment: IComment }) {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   const createMarkup = (html: any) => ({ __html: html });
+
   return (
     <div key={comment.id} className="text-zinc-400 mt-10 w-full">
-      <span className="text-white">
-        {comment.by} {timeAgo(comment.time)}
-      </span>
-      <div style={{ overflowX: "auto" }}>
-        <p dangerouslySetInnerHTML={createMarkup(comment.text)} />
+      <div className="flex items-center">
+        <span className="text-white mr-2">
+          {comment.by} {timeAgo(comment.time)}
+        </span>
+        {comment?.text?.length > 100 && (
+          <button className="text-blue-500" onClick={toggleExpand}>
+            {isExpanded ? "Hide" : "Show more"}
+          </button>
+        )}
       </div>
-      {comment.children && (
+      <div style={{ overflowX: "auto" }}>
+        <p
+          dangerouslySetInnerHTML={createMarkup(
+            isExpanded ? comment.text : `${comment.text.slice(0, 100)}...`
+          )}
+        />
+      </div>
+      {comment.children && isExpanded && (
         <div style={{ marginLeft: "20px" }}>
           {comment.children.map((child) => (
             <div className="mt-[-25px]" key={child.id}>
